9f77f581f856aad4e1a34abbc9ad3074feef5534
arthurcdesouza <arthur.souza@grad.iprj.uerj.com>
2025-08-28 01:30:37 -0300

    corrigida a funçao sorted_merge_3way para passar nos dois testes

--- alg/merge/sorted_merge_3way.cpp
+++ alg/merge/sorted_merge_3way.cpp
@@ -1,21 +1,46 @@
-/* R. Fabbri, 2024 */
 #include "sorted_merge_3way.h"
-#include <climits>
+#include <limits.h>
 
-bool
-sorted_merge_3way(
-    const int *list_a, int na,
-    const int *list_b, int nb,
-    const int *list_c, int nc,
-    int *list_abc)
-{
+bool sorted_merge_3way(const int *list_a, int na,
+                       const int *list_b, int nb,
+                       const int *list_c, int nc,
+                       int *list_abc) {
+    for (int i = 0; i < na - 1; ++i) {
+        if (list_a[i] > list_a[i + 1]) {
+            return false;
+        }
+    }
 
-  /* 
+    for (int i = 0; i < nb - 1; ++i) {
+        if (list_b[i] > list_b[i + 1]) {
+            return false;
+        }
+    }
 
-   Entrar coom aqui 
-   <++>
-   
-  */
+    for (int i = 0; i < nc - 1; ++i) {
+        if (list_c[i] > list_c[i + 1]) {
+            return false;
+        }
+    }
 
-  return false; // stub
+    int i = 0, j = 0, k = 0, l = 0;
+
+    while (i < na || j < nb || k < nc) {
+        int val_a = (i < na) ? list_a[i] : INT_MAX;
+        int val_b = (j < nb) ? list_b[j] : INT_MAX;
+        int val_c = (k < nc) ? list_c[k] : INT_MAX;
+
+        if (val_a <= val_b && val_a <= val_c) {
+            list_abc[l++] = val_a;
+            i++;
+        } else if (val_b <= val_a && val_b <= val_c) {
+            list_abc[l++] = val_b;
+            j++;
+        } else {
+            list_abc[l++] = val_c;
+            k++;
+        }
+    }
+ 
+    return true; 
 }
