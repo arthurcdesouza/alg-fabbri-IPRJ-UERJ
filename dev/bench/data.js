2c4174a7bcca2845078cca1fbce6acb436c25854
github-action-benchmark <github@users.noreply.github.com>
2025-08-29 12:26:54 +0000

    add C++ Benchmark Results (googlecpp) benchmark result for 38588667f585c8a55061384c4b14877b78af9f9d

---
+++ dev/bench/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1756470414236,
+  "repoUrl": "https://github.com/arthurcdesouza/alg-fabbri-IPRJ-UERJ",
+  "entries": {
+    "C++ Benchmark Results": [
+      {
+        "commit": {
+          "author": {
+            "email": "arthur.souza@grad.iprj.uerj.br",
+            "name": "arthurcdesouza",
+            "username": "arthurcdesouza"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "38588667f585c8a55061384c4b14877b78af9f9d",
+          "message": "Update benchmark.yml",
+          "timestamp": "2025-08-29T09:19:17-03:00",
+          "tree_id": "7e277843a5049e4ca50086d7c1ace3fb6c8de400",
+          "url": "https://github.com/arthurcdesouza/alg-fabbri-IPRJ-UERJ/commit/38588667f585c8a55061384c4b14877b78af9f9d"
+        },
+        "date": 1756470413551,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/0",
+            "value": 21.044769387597547,
+            "unit": "ns/iter",
+            "extra": "iterations: 32567611\ncpu: 21.0420107879574 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/1",
+            "value": 31.272232662496503,
+            "unit": "ns/iter",
+            "extra": "iterations: 22676074\ncpu: 31.26864840007137 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/2",
+            "value": 46.27818403681844,
+            "unit": "ns/iter",
+            "extra": "iterations: 15608268\ncpu: 46.27732570968156 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/10",
+            "value": 181.8976516060457,
+            "unit": "ns/iter",
+            "extra": "iterations: 3838155\ncpu: 181.88249640777914 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/50",
+            "value": 856.0439974543813,
+            "unit": "ns/iter",
+            "extra": "iterations: 818661\ncpu: 856.0177289500792 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/100",
+            "value": 1834.3061629393083,
+            "unit": "ns/iter",
+            "extra": "iterations: 385076\ncpu: 1834.173191785518 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/500",
+            "value": 8886.316690841544,
+            "unit": "ns/iter",
+            "extra": "iterations: 78594\ncpu: 8885.918861490694 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/1000",
+            "value": 17743.852153904452,
+            "unit": "ns/iter",
+            "extra": "iterations: 39115\ncpu: 17743.218304998074 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/10000",
+            "value": 321574.1412352427,
+            "unit": "ns/iter",
+            "extra": "iterations: 2202\ncpu: 321545.52134423255 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/100000",
+            "value": 3257401.5767442086,
+            "unit": "ns/iter",
+            "extra": "iterations: 215\ncpu: 3256935.493023257 ns\nthreads: 1"
+          },
+          {
+            "name": "sorted_merge_3way_fixture/BM_sorted_merge_3way/1000000",
+            "value": 32021766.454545546,
+            "unit": "ns/iter",
+            "extra": "iterations: 22\ncpu: 32019490.59090909 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
